00801e973d2fe606e69ae73c1d6bca1e6ab1515e
Andrew Ford <andrew.ford@lambdatechllc.com>
2017-03-28 22:54:17 -0400

    Fix dispatching to properly pick the best found handler (do I need to abort compilation if multiple handlers are found)

--- domains/messaging/dispatcher.hpp
+++ domains/messaging/dispatcher.hpp
@@ -5,109 +5,186 @@
 #include <domains/utils/parameter_pack.hpp>
 
 #include <experimental/memory_resource>
+#include <experimental/vector>
 
 #include <algorithm>
 #include <memory>
-#include <vector>
 
 namespace domains {
 namespace details_ {
-template <class Impl, class T, class = void>
-struct message_dispatch_helper {
-   static void dispatch(Impl &, T const &) noexcept {
-   }
-};
-
-template <class Impl, class T>
-struct message_dispatch_helper<
-    Impl, T, std::void_t<decltype(std::declval<Impl>()(std::declval<T const &>()))>> {
-   static auto dispatch(Impl &handle, T const &t) noexcept(noexcept(handle(t)))
-       -> decltype(handle(t)) {
-      return handle(t);
-   }
-};
+template <class, class = void, class = void>
+struct is_callable_helper : std::false_type {};
 
 template <class T>
-struct message_handler_base {
-   virtual void handle(T const &t) noexcept = 0;
-};
+struct is_callable_helper<T, void, std::void_t<std::result_of_t<T>>> : std::true_type {};
 
-template <class... T>
-struct base_message_handler : message_handler_base<T>... {};
-
-template <class, class, class...>
-class message_handler;
-
-template <class Impl, class... Ts>
-class message_handler<Impl, parameter_pack<Ts...>> : public base_message_handler<Ts...> {
-   Impl impl;
-
-public:
-   explicit message_handler(Impl &&impl) noexcept(
-       std::is_nothrow_constructible<Impl, decltype(std::forward<Impl>(impl))>::value)
-      : impl(std::forward<Impl>(impl)) {
-   }
-
-protected:
-   inline Impl &handler() noexcept {
-      return impl;
-   }
-
-   inline Impl const &handler() const noexcept {
-      return impl;
-   }
-};
-
-template <class Impl, class... Messages, class T, class... Ts>
-class message_handler<Impl, parameter_pack<Messages...>, T, Ts...>
-    : public message_handler<Impl, parameter_pack<Messages...>, Ts...> {
-public:
-   using message_handler<Impl, parameter_pack<Messages...>, Ts...>::message_handler;
-
-   virtual void handle(T const &t) noexcept final override {
-      message_dispatch_helper<Impl, T>::dispatch(this->handler(), t);
-   }
-};
+template <class T, class R>
+struct is_callable_helper<T, R, std::void_t<std::result_of_t<T>>>
+    : std::is_convertible<std::result_of_t<T>, R> {};
 }
 
+template <class, class = void>
+struct is_callable;
+
+template <class T, class R>
+struct is_callable : details_::is_callable_helper<T, R> {};
+
+template <class T, class R = void>
+constexpr bool is_callable_v = is_callable<T, R>::value;
+
+namespace details_ {
+template <class F>
+class dispatch_holder : public F {
+public:
+   using F::operator();
+
+   explicit dispatch_holder(F &&f) : F{std::forward<F>(f)} {
+   }
+};
+
+template <class R, class... Args>
+class dispatch_holder<R (*)(Args...)> {
+   using F = R (*)(Args...);
+   F f;
+
+public:
+   explicit dispatch_holder(F dispatcher) noexcept : f{dispatcher} {
+   }
+
+   R operator()(Args... args) const noexcept(noexcept(f(args...))) {
+      return f(args...);
+   }
+};
+
 template <class...>
-struct single_dispatcher;
-template <class...>
-struct multi_dispatcher;
+struct single_dispatcher_impl;
 
 template <class T>
-struct single_dispatcher<T> : T {
-   using T::operator();
-
-   explicit single_dispatcher(T &&t) noexcept(
-       std::is_nothrow_constructible<T, decltype(std::forward<T>(t))>::value)
-      : T(std::forward<T>(t)) {
-   }
+struct single_dispatcher_impl<T> : dispatch_holder<T> {
+   using dispatch_holder<T>::dispatch_holder;
+   using dispatch_holder<T>::operator();
 };
 
 template <class T, class... Ts>
-struct single_dispatcher<T, Ts...> : T, single_dispatcher<Ts...> {
-   using T::operator();
-   using single_dispatcher<Ts...>::operator();
+struct single_dispatcher_impl<T, Ts...> : dispatch_holder<T>, single_dispatcher_impl<Ts...> {
+   using dispatch_holder<T>::operator();
+   using details_::single_dispatcher_impl<Ts...>::operator();
 
-   explicit single_dispatcher(T &&t, Ts &&... ts) noexcept(
-       std::is_nothrow_constructible<T, decltype(std::forward<T>(t))>::value
-           &&std::is_nothrow_constructible<single_dispatcher<Ts...>,
-                                           decltype(std::forward<Ts>(ts))...>::value)
-      : T(std::forward<T>(t)), single_dispatcher<Ts...>(std::forward<Ts>(ts)...) {
+protected:
+   explicit single_dispatcher_impl(T &&t, Ts &&... ts)
+      : dispatch_holder<T>{std::forward<T>(t)},
+        single_dispatcher_impl<Ts...>{std::forward<Ts>(ts)...} {
+   }
+};
+}
+
+template <class... Fs>
+struct single_dispatcher final : details_::single_dispatcher_impl<std::decay_t<Fs>...> {
+   using details_::single_dispatcher_impl<std::decay_t<Fs>...>::operator();
+
+   explicit single_dispatcher(std::decay_t<Fs> &&... fs)
+      : details_::single_dispatcher_impl<std::decay_t<Fs>...>{std::move(fs)...} {
    }
 };
 
-template <class... Ts>
-single_dispatcher<Ts...> make_single_dispatcher(Ts &&... ts) noexcept(
-    std::is_nothrow_constructible<single_dispatcher<Ts...>,
-                                  decltype(std::forward<Ts>(ts))...>::value) {
-   return single_dispatcher<Ts...>(std::forward<Ts>(ts)...);
+template <class... Fs>
+single_dispatcher<Fs...> make_single_dispatcher(Fs... fs) {
+   return single_dispatcher<Fs...>{std::move(fs)...};
 }
 
-template <class... Ts>
-class multi_dispatcher final {
-   using handler_type = details_::base_message_handler<Ts...>;
+namespace details_ {
+template <class Signature>
+struct handler;
+
+template <class R, class... A>
+struct handler<R(A...)> {
+   virtual ~handler() noexcept {
+   }
+   virtual R handle(A...) = 0;
+};
+
+template <class... Signatures>
+struct base_handler : handler<Signatures>... {};
+
+template <class, class, class...>
+struct handler_impl;
+
+template <class Impl, class... Signatures>
+struct handler_impl<Impl, parameter_pack<Signatures...>> : base_handler<Signatures...> {
+   explicit handler_impl(Impl &&i) noexcept(std::is_move_constructible<Impl>::value)
+      : impl(std::move(i)) {
+   }
+
+protected:
+   Impl &get() noexcept {
+      return impl;
+   }
+   Impl const &get() const noexcept {
+      return impl;
+   }
+
+private:
+   Impl impl;
+};
+
+template <class Impl, class... Signatures, class R, class... A, class... Tail>
+struct handler_impl<Impl, parameter_pack<Signatures...>, R(A...), Tail...>
+    : handler_impl<Impl, parameter_pack<Signatures...>, Tail...> {
+   using handler_impl<Impl, parameter_pack<Signatures...>, Tail...>::handler_impl;
+
+   virtual R handle(A... a) override final {
+      return std::invoke(this->get(), a...);
+   }
+};
+
+template <bool, template <class, class> class, class, class>
+struct type_count {
+   constexpr static size_t value = 0;
+};
+
+template <template <class, class> class Filter>
+struct type_count<true, Filter, parameter_pack<>, parameter_pack<>> {
+   constexpr static size_t value = 1;
+};
+
+template <template <class, class> class Filter, class A, class... As, class B, class... Bs>
+struct type_count<true, Filter, parameter_pack<A, As...>, parameter_pack<B, Bs...>> {
+   constexpr static size_t value =
+       (Filter<std::decay_t<A>, std::decay_t<B>>::value ? 1 : 0) +
+       type_count<true, Filter, parameter_pack<As...>, parameter_pack<Bs...>>::value;
+};
+
+template <class, class...>
+class handler_picker {
+public:
+   constexpr static size_t count = 0;
+   using handler_type = void;
+   using result_type = void;
+};
+
+template <class... InputArgs, class R, class... A, class... Tail>
+class handler_picker<parameter_pack<InputArgs...>, R(A...), Tail...> {
+   using next = handler_picker<parameter_pack<InputArgs...>, Tail...>;
+   using handler_base = handler<R(A...)>;
+   using handler_func = R (handler_base::*)(A...);
+
+   constexpr static bool is_valid =
+       is_callable_v<handler_func(handler<R(A...)> *, InputArgs...), R>;
+   constexpr static size_t current_count =
+       type_count<is_valid, std::is_same, parameter_pack<InputArgs...>,
+                  parameter_pack<A...>>::value;
+   constexpr static bool use_mine = current_count > next::count;
+
+public:
+   constexpr static size_t count = use_mine ? current_count : next::count;
+   using handler_type = std::conditional_t<use_mine, handler_base, typename next::handler_type>;
+   using result_type = std::conditional_t<use_mine, R, typename next::result_type>;
+};
+}
+
+template <class... Signatures>
+class multi_dispatcher {
+   using handler_type = details_::base_handler<Signatures...>;
    class handler_deleter final {
       std::size_t size;
       std::size_t align;
@@ -127,46 +204,54 @@ class multi_dispatcher final {
    };
    using handler_ptr = std::unique_ptr<handler_type, handler_deleter>;
 
-   std::vector<handler_ptr, std::experimental::pmr::polymorphic_allocator<handler_ptr>> handlers;
+   std::experimental::pmr::vector<handler_ptr> handlers;
 
-   std::experimental::pmr::memory_resource *resource() const {
+   std::experimental::pmr::memory_resource *resource() const noexcept {
       return handlers.get_allocator().resource();
    }
 
 public:
-   explicit multi_dispatcher(std::experimental::pmr::memory_resource *resource =
-                                 std::experimental::pmr::get_default_resource()) noexcept
-       : handlers(resource) {
+   explicit multi_dispatcher(std::experimental::pmr::memory_resource *resource) noexcept
+       : handlers{resource == nullptr ? std::experimental::pmr::get_default_resource() : resource} {
+   }
+
+   explicit multi_dispatcher() noexcept
+       : multi_dispatcher(std::experimental::pmr::get_default_resource()) {
+   }
+
+   // Currently, the signatures must match exactly.  Otherwise, it'll fail to resolve as
+   // dispatchable. We should first check to see if we have an exact match.
+   template <class... A>
+   void operator()(A... a) {
+      using match = details_::handler_picker<parameter_pack<A...>, Signatures...>;
+      using base = typename match::handler_type;
+      static_assert(!std::is_void<std::remove_pointer_t<base>>::value,
+                    "There was no handler found to support the called arguments.");
+      std::for_each(handlers.begin(), handlers.end(), [&](handler_ptr &handler) {
+         static_cast<base *>(handler.get())->handle(a...);
+      });
    }
 
    template <class F>
-   void add_handler(F &&f) {
-      using Impl = details_::message_handler<F, parameter_pack<Ts...>, Ts...>;
+   void add_handler(F f) {
+      using Impl =
+          details_::handler_impl<std::decay_t<F>, parameter_pack<Signatures...>, Signatures...>;
 
       Impl *const handler = static_cast<Impl *>(resource()->allocate(sizeof(Impl), alignof(Impl)));
       new (handler) Impl(std::forward<F>(f));
       handlers.emplace_back(handler, handler_deleter{sizeof(Impl), alignof(Impl), resource()});
    }
 
-   template <class... Fs>
-   void add_handlers(Fs &&... fs) {
-      add_handler(make_single_dispatcher(std::forward<Fs>(fs)...));
+   void shrink_to_fit() {
+      handlers.shrink_to_fit();
    }
 
-   template <class U>
-   friend std::enable_if_t<in<U, parameter_pack<Ts...>>::value>
-   dispatch(multi_dispatcher<Ts...> &dispatcher, U const &u) noexcept {
-      for (auto &handler : dispatcher.handlers) {
-         static_cast<details_::message_handler_base<U> *>(handler.get())->handle(u);
-      }
+   typename std::experimental::pmr::vector<handler_ptr>::size_type size() const noexcept {
+      return handlers.size();
+   }
+
+   bool empty() const noexcept {
+      return handlers.empty();
    }
 };
-
-template <class F, class T>
-auto dispatch(F &f,
-              T const &t) noexcept(noexcept(details_::message_dispatch_helper<F, T>::dispatch(f,
-                                                                                              t)))
-    -> decltype(details_::message_dispatch_helper<F, T>::dispatch(f, t)) {
-   return details_::message_dispatch_helper<F, T>::dispatch(f, t);
-}
 }
--- unit_tests/domains/messaging/dispatcher.cpp
+++ unit_tests/domains/messaging/dispatcher.cpp
@@ -26,8 +26,6 @@ struct C : A {
    }
 };
 
-struct D final {};
-
 TEST_CASE("Single dispatcher", "[dispatcher]") {
    auto target = make_single_dispatcher(
        [](std::experimental::string_view s) { return s.size(); }, [](int i) { return i; },
@@ -52,50 +50,46 @@ TEST_CASE("Multi dispatcher", "[dispatcher]") {
    size_t a_invocations = 0;
    size_t b_invocations = 0;
    size_t c_invocations = 0;
-   size_t d_invocations = 0;
-   size_t int_invocations = 0;
-   multi_dispatcher<std::string, B, C, D> target;
-   target.add_handlers([&](std::experimental::string_view) { ++sv_invocations; },
-                       [&](A const &) { ++a_invocations; }, [&](C const &) { ++c_invocations; });
-   target.add_handler([&](int) { ++int_invocations; });
+   size_t other_invocations = 0;
+   multi_dispatcher<void(std::experimental::string_view), void(B const &), void(C const &)> target;
+   target.add_handler(make_single_dispatcher(
+       [&](std::experimental::string_view) { ++sv_invocations; },
+       [&](A const &) { ++a_invocations; }, [&](C const &) { ++c_invocations; }));
+   target.add_handler([&](auto const &) { ++other_invocations; });
 
-   SECTION("can route to handler on convertible type") {
-      dispatch(target, std::string{"help"});
+   SECTION("can route to handler on same type") {
+      target(std::experimental::string_view{"help"});
       REQUIRE(sv_invocations == 1);
       REQUIRE(a_invocations == 0);
       REQUIRE(b_invocations == 0);
       REQUIRE(c_invocations == 0);
-      REQUIRE(d_invocations == 0);
-      REQUIRE(int_invocations == 0);
+      REQUIRE(other_invocations == 1);
+   }
+
+   SECTION("can route to handler on convertible type") {
+      target("help");
+      REQUIRE(sv_invocations == 1);
+      REQUIRE(a_invocations == 0);
+      REQUIRE(b_invocations == 0);
+      REQUIRE(c_invocations == 0);
+      REQUIRE(other_invocations == 1);
    }
 
    SECTION("can route to handler on base type") {
-      dispatch(target, B{});
+      target(B{});
       REQUIRE(sv_invocations == 0);
       REQUIRE(a_invocations == 1);
       REQUIRE(b_invocations == 0);
       REQUIRE(c_invocations == 0);
-      REQUIRE(d_invocations == 0);
-      REQUIRE(int_invocations == 0);
+      REQUIRE(other_invocations == 1);
    }
 
    SECTION("can route to handler on child type") {
-      dispatch(target, C{});
+      target(C{});
       REQUIRE(sv_invocations == 0);
       REQUIRE(a_invocations == 0);
       REQUIRE(b_invocations == 0);
       REQUIRE(c_invocations == 1);
-      REQUIRE(d_invocations == 0);
-      REQUIRE(int_invocations == 0);
-   }
-
-   SECTION("will ignore unhandle types") {
-      REQUIRE_NOTHROW(dispatch(target, D{}));
-      REQUIRE(sv_invocations == 0);
-      REQUIRE(a_invocations == 0);
-      REQUIRE(b_invocations == 0);
-      REQUIRE(c_invocations == 0);
-      REQUIRE(d_invocations == 0);
-      REQUIRE(int_invocations == 0);
+      REQUIRE(other_invocations == 1);
    }
 }
